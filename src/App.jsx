97f3149d9368bc2bc76a149e424fee38a6c0cba6
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-07 15:32:13 +0300

    feat: add timer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="easy" targetTime={1} />
+        <TimerChallenge title="medium" targetTime={2} />
+        <TimerChallenge title="Hard" targetTime={4} />
+        <TimerChallenge title="Expert" targetTime={10} />
+      </div>
     </>
   );
 }
