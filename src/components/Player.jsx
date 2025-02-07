b01e28f3b17002c88100de661d95787824d7b021
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-07 14:37:26 +0300

    feat: enable two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 export default function Player() {
+  const [playerName, setPlayerName] = useState('')
+  const [nameSaved, setNameSaved] = useState('')
+  function handleChange(event) {
+    setNameSaved(false)
+    setPlayerName(event.target.value)
+  }
+
+  function handleClick(params) {
+    setNameSaved(true)
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {nameSaved ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={setNameSaved}>Set Name</button>
       </p>
     </section>
   );
